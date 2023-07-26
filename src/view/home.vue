0dbcd956876a86c73bd52e2a2476c87e50c5ef8e
Amytzhang <2429323352@qq.com>
2023-07-26 22:33:07 +0800

    样式一

--- src/view/home.vue
+++ src/view/home.vue
@@ -1,5 +1,31 @@
 <template>
-  <div>home</div>
+  <div class="home">
+    <header>header content</header>
+    <div class="home-content">
+      <div class="ts-study">ts学习</div>
+    </div>
+  </div>
 </template>
 <script lang="ts" setup></script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  background-color: black;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+}
+.home-content {
+  flex: 1;
+  padding: 1.5rem 2rem;
+  overflow: hidden;
+}
+.ts-study{
+  width: 10rem;
+  height: 5rem;
+  border:  10px solid hsla(0, 0%, 100%, .5);
+  background: white;
+  background-clip: padding-box;
+}
+</style>
