a848c5e6ef5ee3893ab0cab1b93e89ee8b95701b
Amytzhang <2429323352@qq.com>
2023-07-26 22:36:01 +0800

    提交修改

--- src/view/TsStatus/index.vue
+++ src/view/TsStatus/index.vue
@@ -1,49 +1,26 @@
 <template>
-  <el-tabs v-model="activeName" class="demo-tabs" @tab-change="handleClick">
-    <el-tab-pane label="ts基础1" name="first">
-      <div class="content">
-        <TsBasis1></TsBasis1>
-      </div>
-    </el-tab-pane>
-    <el-tab-pane label="ts基础2" name="second">
-      <div class="content">
-        <TsBasis2></TsBasis2>
-      </div>
-    </el-tab-pane>
-    <el-tab-pane label="Role" name="third">Role</el-tab-pane>
-    <el-tab-pane label="Task" name="fourth">Task</el-tab-pane>
-  </el-tabs>
+  <div>
+    <div class="content">
+      <TsBasis1></TsBasis1>
+    </div>
+    <div class="content">
+      <TsBasis2></TsBasis2>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue"
-import { ref } from "vue"
-import type { TabsPaneContext, TabPaneName } from "element-plus"
-import TsBasis1 from "./Ts_Basis1.vue"
-import TsBasis2 from "./Ts_Basis2.vue"
-export default defineComponent({
-  name: "tsVue3",
-  components: {
-    TsBasis1,
-    TsBasis2
-  },
-  setup() {
-    const activeName = ref("first")
-    const handleClick = (tab: TabPaneName) => {
-      console.log(tab, activeName.value)
-    }
-    return {
-      activeName,
-      handleClick
-    }
-  }
-})
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+import { ref } from 'vue'
+import TsBasis1 from './Ts_Basis1.vue'
+import TsBasis2 from './Ts_Basis2.vue'
+
 </script>
 <style lang="css" scoped>
-.content {
-  width: 60vw;
-  height: 90vh;
-  overflow: auto;
-  overflow-y: scroll;
-}
+  .content{
+    width: 60vw;
+    height: 90vh;
+    overflow: auto;
+    overflow-y: scroll;
+  }
 </style>
