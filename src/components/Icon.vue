a9c3d74b66b7cb77723cac416865f7729b502932
Amytzhang <2429323352@qq.com>
2023-08-20 11:48:16 +0800

    icon try

---
+++ src/components/Icon.vue
@@ -0,0 +1,30 @@
+<template>
+  <component :is="icon" class="ct-w-5 ct-h-5" />
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import * as heroIcons from '@heroicons/vue/outline'
+export default defineComponent({
+  name: 'icon',
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props, ctx) {
+    let name = ''
+
+    props.name.split('-').forEach((v) => {
+      name += v[0].toUpperCase() + v.substr(1)
+    })
+
+    const icon = heroIcons[name + 'Icon']
+
+    return {
+      icon
+    }
+  }
+})
+</script>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import './main.css'
 import App from './App.vue'
 import router from './router'
 import { store } from './store/index'
+import icon from '@/components/Icon.vue'
+
+
 const app = createApp(App)
+//注册
+app.component('Icon', icon)
 
 app.use(store).use(router).mount('#app')
